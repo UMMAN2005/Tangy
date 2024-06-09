d82a26e8b762315136af17ea5b3f65c613c33dfd
UMMANCODE <ummanmm@code.edu.az>
2024-06-09 20:32:34 +0400

    files added

---
+++ Tangy_Models/StripePaymentDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy_Models {
+  public class StripePaymentDTO {
+    public StripePaymentDTO() {
+      SuccessUrl = "OrderConfirmation";
+      CancelUrl = "Summary";
+    }
+    [Required]
+    public OrderDTO? Order { get; set; }
+    [Required]
+    public string? SuccessUrl { get; set; }
+    [Required]
+    public string? CancelUrl { get; set; }
+  }
+}
